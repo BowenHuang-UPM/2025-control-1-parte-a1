3d8cafae05a3311e5fb49c90f0014bbf760d319f
Bowen Huang <55832958+BowenHuang-UPM@users.noreply.github.com>
2025-11-06 17:52:47 +0100

    Update CruiseControlTest.java

--- src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
+++ src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
@@ -1,5 +1,22 @@
 package es.upm.grise.profunduzacion.cruiseController;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 class CruiseControlTest {
 	
+	@Test
+	void setSpeedSetNegativaTest() {
+		assertThrows(IncorrectSpeedSetException.class,() ->{
+			
+		});
+	}
+	
+	@Test
+	void speedSetAboveSpeedLimitTest() {
+		assertThrows(SpeedSetAboveSpeedLimitException.class,() ->{
+			
+		});
+	}
 }
